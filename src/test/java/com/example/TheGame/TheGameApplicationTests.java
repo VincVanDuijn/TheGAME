39ed160002dba9fbee8b108fa8c286d80d158a3c
Vincent van Duijn <vincent.van.duijn@devoteam.com>
2022-12-23 22:23:40 +0100

    First commit

---
+++ src/test/java/com/example/TheGame/TheGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TheGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
